4cd8a588ff38f01c0046c4080ab59bc79e5e805c
Navneet Singh <singhnavneet@salesforce.com>
2023-12-20 14:03:38 +0530

    some changes

--- nr_interactions.py
+++ nr_interactions.py
@@ -5,12 +5,17 @@ load_dotenv()
 
 nr_license_key = os.getenv("PROD_NR_LICENSE_KEY")
 
+data = {'server': 'dub-ts', 'line': '0/2/1', 'port': 5002,
+        'device': 'dub-dst-a', 'last_tested': '2023-12-20 06:52:34.718842'}
+
 
 def update_terminal_server_access():
     metric_client = MetricClient(nr_license_key)
     batch = []
-    key = "terminal_server_access_test.test"
+    key = 'terminal_server_access_test'
     metric = GaugeMetric(key, 20)
     batch.append(metric)
     response = metric_client.send_batch(batch)
     response.raise_for_status()
+
+update_terminal_server_access()
