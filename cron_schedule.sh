4c5c2e2694db68409b1aaf8d793de49c6aeff0ee
Navneet Singh <singhnavneet@salesforce.com>
2024-07-03 12:57:52 +0530

    some changes

--- cron_schedule.sh
+++ cron_schedule.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/bin/python /home/singhnavneet.su/console-access-automation/main.py
+/home/singhnavneet.su/console-access-automation/venv/bin/python3.9 /home/singhnavneet.su/console-access-automation/main.py
 
 # */3 * * * * /home/singhnavneet.su/console-access-automation/cron_schedule.sh >> /home/singhnavneet.su/console-access-automation/cron.log 2>&1
