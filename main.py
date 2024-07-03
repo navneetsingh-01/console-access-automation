864a669991d66ba58205cabf2138c59a4e536685
Navneet Singh <singhnavneet@salesforce.com>
2024-07-03 11:18:01 +0530

    some changes

--- main.py
+++ main.py
@@ -303,6 +303,7 @@ try:
                                 print("VAL: ", val)
                                 l = val.find(sitecode)
                                 r = val.find('#')
+                                print("Line: " + line)
                                 while l >= 0 and ((val[l] >= 'a' and val[l] <= 'z') or val[l] == '-'):
                                     l -= 1
                                 print(line)
