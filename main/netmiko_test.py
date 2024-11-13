7fb02fbd7560b1fe3edc0d09cf4778e406a6219d
Navneet Singh <singhnavneet@salesforce.com>
2024-11-13 13:04:16 +0530

    some changes

--- main/netmiko_test.py
+++ main/netmiko_test.py
@@ -1,17 +1,16 @@
 import os
 import paramiko
-from netmiko  import ConnectHandler
+from netmiko import ConnectHandler
+from getpass import getpass
 
-net_connect = ConnectHandler(
-    device_type="cisco_ios",
-    host="syd4-ts",
-    username=os.getenv("TACACS_USERNAME"),
-    password=os.getenv("TACACS_PASSWORD"),
-)
 
-try: 
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+try:
+    net_connect = ConnectHandler(
+        device_type="cisco_ios",
+        host="syd4-ts",
+        username=os.getenv("TACACS_USERNAME"),
+        password=getpass(),
+    )
     print(net_connect.find_prompt())
     net_connect.disconnect()
 except Exception as e:
