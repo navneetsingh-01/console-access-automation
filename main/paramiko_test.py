446f9b7a27c609f9385e979b324306ef590895cf
Navneet Singh <singhnavneet@salesforce.com>
2024-11-20 12:00:49 +0530

    some changes

--- main/paramiko_test.py
+++ main/paramiko_test.py
@@ -1,21 +1,43 @@
 import paramiko
 import time
+import os
 
 # Create SSH client
-client = paramiko.SSHClient()
+try:
 
-# Set policy to automatically add hosts to known hosts file
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client = paramiko.SSHClient()
 
-# Connect to the remote host
-client.connect(
-    hostname="fra4-ts",
-    username="singhnavneet.su",
-    password="mC$<4?7H{nocR\Emilj=",
-)
+    # Set policy to automatically add hosts to known hosts file
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-# Open an interactive SSH session
-ssh_client = client.invoke_shell()
+    # Connect to the remote host
+    client.connect(
+        hostname="fra4-ts",
+        username=os.getenv("TACACS_USERNAME"),
+        password=os.getenv("TACACS_PASSWORD"),
+    )
 
-# Send command
-ssh_client.send("sh ip int bri\n")
+    # Open an interactive SSH session
+    ssh_client = client.invoke_shell()
+
+    # Send command
+    ssh_client.send("sh ip int bri\n")
+
+    # Wait for the command to be finished
+    time.sleep(3)
+
+    # Receive and process command output
+    output = ssh_client.recv(65000)
+    print(output.decode("ascii"))
+
+    # Close the SSH session
+    ssh_client.close()
+
+    # Close the connection
+    client.close()
+
+except paramiko.AuthenticationException:
+    print("Authentication failed. Please verify your credentials.")
+
+except Exception as e:
+    print("Something went wrong: " + str(e))
