306d4f82f360f43e464f323a33d43af423275698
Navneet Singh <singhnavneet@salesforce.com>
2024-11-20 12:30:03 +0530

    some changes

--- main/paramiko_test.py
+++ main/paramiko_test.py
@@ -13,28 +13,29 @@ try:
     # Connect to the remote host
     client.connect(
         hostname="fra4-ts",
-        username=os.getenv("NETADMIN_USERNAME"),
-        password=os.getenv("NETADMIN_PASSWORD"),
+        username=os.getenv("TACACS_USERNAME"),
+        password=os.getenv("TACACS_PASSWORD"),
+        port=22
     )
 
-    # Open an interactive SSH session
-    ssh_client = client.invoke_shell()
+    # # Open an interactive SSH session
+    # ssh_client = client.invoke_shell()
 
-    # Send command
-    ssh_client.send("sh ip int bri\n")
+    # # Send command
+    # ssh_client.send("sh ip int bri\n")
 
-    # Wait for the command to be finished
-    time.sleep(3)
+    # # Wait for the command to be finished
+    # time.sleep(3)
 
-    # Receive and process command output
-    output = ssh_client.recv(65000)
-    print(output.decode("ascii"))
+    # # Receive and process command output
+    # output = ssh_client.recv(65000)
+    # print(output.decode("ascii"))
 
-    # Close the SSH session
-    ssh_client.close()
+    # # Close the SSH session
+    # ssh_client.close()
 
-    # Close the connection
-    client.close()
+    # # Close the connection
+    # client.close()
 
 except paramiko.AuthenticationException:
     print("Authentication failed. Please verify your credentials.")
